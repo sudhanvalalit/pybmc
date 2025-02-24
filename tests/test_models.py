aad651edf4c01e29419f38bc74f2b8b16bbf077d
Kyle Godbey <kylegodbey@gmail.com>
2025-02-24 16:30:46 -0500

    Update models to use numpy arrays for domain and range
    Enforce that Model's domain and output are numpy arrays and rename them to x and y.
    
    * Import numpy as np in `pybmc/models.py` and `tests/test_models.py`.
    * Update `Model` class in `pybmc/models.py`:
      - Enforce that `x` and `y` are numpy arrays in the `__init__` method.
      - Change the `domain` attribute to `x` and `range` attribute to `y`.
      - Add documentation explaining that `x` and `y` correspond to the domain and range of the models.
    * Update `TestModel` class in `tests/test_models.py`:
      - Initialize `x` and `y` as numpy arrays in the `setUp` method.
      - Change the `domain` attribute to `x` and `range` attribute to `y` in the test methods.
      - Use `np.testing.assert_array_equal` to test `x` and `y` attributes.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,18 +1,17 @@
 import unittest
+import numpy as np
 from pybmc.models import Model
 
 
 class TestModel(unittest.TestCase):
     def setUp(self):
-        self.model = Model("test_model")
+        self.model = Model("test_model", np.array([1, 2, 3, 4, 5]), np.array([10, 20, 30, 40, 50]))
 
-    def test_domain(self):
-        self.model.domain = [1, 2, 3, 4, 5]
-        self.assertEqual(self.model.domain, [1, 2, 3, 4, 5])
+    def test_x(self):
+        np.testing.assert_array_equal(self.model.x, np.array([1, 2, 3, 4, 5]))
 
-    def test_range(self):
-        self.model.range = [10, 20, 30, 40, 50]
-        self.assertEqual(self.model.range, [10, 20, 30, 40, 50])
+    def test_y(self):
+        np.testing.assert_array_equal(self.model.y, np.array([10, 20, 30, 40, 50]))
 
 
 if __name__ == '__main__':
